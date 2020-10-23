fea31db341f4cf51a7a89c5d98617055b5fa19eb
onbgh823 <68522421+onbgh823@users.noreply.github.com>
2020-10-23 18:17:41 +0900

    1023fin

--- js/asan.js
+++ js/asan.js
@@ -16,41 +16,47 @@
 // `<p>${depart}</p>`
 // `<div>${about}</div></div></li>`
 
+
+var usedata;
+$.ajax({
+  type: 'GET',
+  url: 'data/doctors.json',
+  beforeSend: function (xhr) {
+    if (xhr.overrideMimeType) {
+      xhr.overrideMimeType('application/json');
+    }
+  },
+  success:function(data){
+    usedata = data
+  },
+  error:function(abc){
+    alert(abc.status + '오류발생')
+  }
+})
+
+
 $('#container').on('click', '.medicalContent .mediList a', function(e){
   e.preventDefault()
   var url = this.href;
   var part = this.id;
     $("#container > #content").remove();
-    $("#container").load(url + " #content");
-
-    $.ajax({
-      type: 'GET',
-      url: 'data/doctors.json',
-      beforeSend: function (xhr) {
-        if (xhr.overrideMimeType) {
-          xhr.overrideMimeType('application/json');
-        }
-      },
-      success:function(data){
-        var usedata = data[part]
-        var newContent = '';
-        function dataPrint () {
-          for( var i in usedata) {
-            newContent += `<li><div class="img"><img src="${usedata[i].photo}" alt=""></div>`
-            newContent += `<div class="doctorInfo"><strong>${usedata[i].name}</strong>`
-            newContent += `<p>${usedata[i].depart}</p>`
-            newContent += `<div>${usedata[i].about}</div></div></li>`
-          }
-          $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
-        }
-        dataPrint()
-      },
-      error:function(abc){
-        alert(abc.status + '오류발생')
+    $("#container").load(url + " #content", function(){
+      var newContent = '';
+    
+      for( var i in usedata[part]) {
+        newContent += `<li><div class="img"><img src="${usedata[part][i].photo}" alt=""></div>`
+        newContent += `<div class="doctorInfo"><strong>${usedata[part][i].name}</strong>`
+        newContent += `<p>${usedata[part][i].depart}</p>`
+        newContent += `<div>${usedata[part][i].about}</div></div></li>`
       }
-    })
+      $('#content .part1DoctorList').html(`<ul>${newContent}</ul>`)
+
+    });
+
+    
 
 })
+
 
 
   // 헤더박스는 스크롤이벤트시 픽스드됨
